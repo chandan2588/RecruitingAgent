34576223395b86b658cc46d3f147920d08dde350
ravlok <chandan.may2@gmail.com>
2026-02-22 07:53:04 +0530

    Fix: Correct auth import in dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 export const dynamic = "force-dynamic";
 
-import { OrganizationSwitcher, UserButton, auth } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 
 export default async function DashboardLayout({
